da9e10306b7dd9dfdc609a8d36cbfcf4f881a03d
Eduardo Perez Salcedo <a.eduardoperez.fp2019@gmail.com>
2024-01-24 05:38:39 -0400

    feat(Footer): Agregamos creditos

--- src/components/app/Footer/Footer.jsx
+++ src/components/app/Footer/Footer.jsx
@@ -6,31 +6,51 @@ import { Box, Anchor } from '../../../components';
 
 export const Footer = () => {
 	return (
-		<Box className='footer'>
-			<Anchor
-				href='https://www.linkedin.com/in/eduardo-enrique-p%C3%A9rez-salcedo-792136211/'
-				target='_blank'
-				rel='noreferrer'
-				className='footer_network'
-			>
-				GitHub
-			</Anchor>
-			<Anchor
-				href='https://github.com/Eduardo-Perez17'
-				target='_blank'
-				rel='noreferrer'
-				className='footer_network'
-			>
-				Linkedin
-			</Anchor>
-			<Anchor
-				href='mailto:a.eduardoperez.fp2019@gmail.com'
-				target='_blank'
-				rel='noreferrer'
-				className='footer_network'
-			>
-				Gmail
-			</Anchor>
-		</Box>
+		<>
+			<Box className='footer'>
+				<Box className='footer_anchor'>
+					<Anchor
+						href='https://www.linkedin.com/in/eduardo-enrique-p%C3%A9rez-salcedo-792136211/'
+						target='_blank'
+						rel='noreferrer'
+						className='footer_network'
+					>
+						GitHub
+					</Anchor>
+					<Anchor
+						href='https://github.com/Eduardo-Perez17'
+						target='_blank'
+						rel='noreferrer'
+						className='footer_network'
+					>
+						Linkedin
+					</Anchor>
+					<Anchor
+						href='mailto:a.eduardoperez.fp2019@gmail.com'
+						target='_blank'
+						rel='noreferrer'
+						className='footer_network'
+					>
+						Gmail
+					</Anchor>
+				</Box>
+
+				<Box className='footer-credits'>
+					<p>
+						Desarrollado por <strong>Team Dev</strong> y diseño basado en{' '}
+						<strong>
+							<Anchor
+								href='https://midu.dev'
+								target='_blank'
+								rel='noreferrer'
+								className='footer_network'
+							>
+								midu.dev
+							</Anchor>
+						</strong>
+					</p>
+				</Box>
+			</Box>
+		</>
 	);
 };
