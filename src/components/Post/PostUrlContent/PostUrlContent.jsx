0d5b39db9b439e39f3feae59a549059f13385d39
Eduardo Perez Salcedo <a.eduardoperez.fp2019@gmail.com>
2024-02-09 00:51:19 -0400

    feat(Post): le agregamos estilos al texto y a las cajas de codigo

--- src/components/Post/PostUrlContent/PostUrlContent.jsx
+++ src/components/Post/PostUrlContent/PostUrlContent.jsx
@@ -1,4 +1,8 @@
 export const PostUrlContent = ({ postByUrl }) => {
-	
-	return <div dangerouslySetInnerHTML={{ __html: postByUrl?.data?.content }} />;
+	return (
+			<div
+				dangerouslySetInnerHTML={{ __html: postByUrl?.data?.content }}
+				className='post_content_item'
+			/>
+	);
 };
