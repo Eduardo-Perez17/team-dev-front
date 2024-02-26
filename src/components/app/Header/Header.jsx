8eb7b3eb0685911127072e1e30724b7aa0be1b62
Eduardo Perez Salcedo <a.eduardoperez.fp2019@gmail.com>
2024-02-26 07:41:29 -0400

    feat(Header): El header de la app ya es responsive

--- src/components/app/Header/Header.jsx
+++ src/components/app/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 // MUI
 import GitHubIcon from '@mui/icons-material/GitHub';
+import CloseIcon from '@mui/icons-material/Close';
 
 // Path
 import { HOME } from '../../../utils/constants';
@@ -22,8 +23,11 @@ import { Search, Menu } from '../../icons';
 
 export const Header = () => {
 	const [open, setOpen] = useState(false);
+	const [menuMobile, setMenuMobile] = useState(false);
+
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
+	const handleMenuMobile = () => setMenuMobile(!menuMobile);
 
 	return (
 		<>
@@ -33,35 +37,62 @@ export const Header = () => {
 						<img src={LOGO_DARK.img} alt={LOGO_DARK.alt} />
 					</Box>
 				</Link>
-				<Box className='header_search'>
-					<Menu
-						className='header_search_icon_menu'
-						width='50'
-						height='50'
-						fill='#fff'
-					/>
-
-					<Box className='header_search_desktop'>
-						<Box className='header_search_input' onClick={handleOpen}>
-							<Search
-								className='header_search_input_icon'
-								width='25'
-								height='25'
-							/>
-							<Input
-								type='text'
-								placeholder='Buscar...'
-								className='header_search_input_item'
-							/>
+				<Box className={menuMobile ? 'header_menu_mobile' : 'header_search'}>
+					{menuMobile ? (
+						<Box className='header_menu_mobile_item'>
+							<Box onClick={handleMenuMobile} className='header_menu_mobile_item_input'>
+								<CloseIcon />
+								<Box className='header_search_input' onClick={handleOpen}>
+									<Search
+										className='header_search_input_icon'
+										width='25'
+										height='25'
+									/>
+									<Input
+										type='text'
+										placeholder='Buscar...'
+										className='header_search_input_item'
+									/>
+								</Box>
+							</Box>
 						</Box>
+					) : (
+						<>
+							<Menu
+								className='header_search_icon_menu'
+								width='50'
+								height='50'
+								fill='#fff'
+								onClick={handleMenuMobile}
+							/>
 
-						<Box className='header_network'>
-							{/* <img src={SUN.img} alt={SUN.alt} width={35} height={35} /> */}
-							<a href='https://github.com/Eduardo-Perez17'  target='_blank' rel='noreferrer'>
-								<GitHubIcon />
-							</a>
-						</Box>
-					</Box>
+							<Box className='header_search_desktop'>
+								<Box className='header_search_input' onClick={handleOpen}>
+									<Search
+										className='header_search_input_icon'
+										width='25'
+										height='25'
+									/>
+									<Input
+										type='text'
+										placeholder='Buscar...'
+										className='header_search_input_item'
+									/>
+								</Box>
+
+								<Box className='header_network'>
+									{/* <img src={SUN.img} alt={SUN.alt} width={35} height={35} /> */}
+									<a
+										href='https://github.com/Eduardo-Perez17'
+										target='_blank'
+										rel='noreferrer'
+									>
+										<GitHubIcon />
+									</a>
+								</Box>
+							</Box>
+						</>
+					)}
 				</Box>
 			</Box>
 
